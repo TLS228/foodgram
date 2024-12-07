7bafaea58dd5ae2909f5d0d3f19e9fb0789b40be
Nikita Solovey <solovei292000@gmail.com>
2024-12-07 20:34:36 +0300

    full final3

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -6,7 +6,7 @@ from recipes.views import RedirectViewSet
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
     path('api/', include('api.urls'), name='api'),
-    path('redirect/<str:short_code>/',
+    path('s/<str:short_code>/',
          RedirectViewSet.as_view({'get': 'redirect_short_link'}),
          name='short_link_redirect'),
 ]
